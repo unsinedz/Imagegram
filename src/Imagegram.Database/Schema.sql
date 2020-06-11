c2563cdc084522e3828cf53921eb652a7e5aa783
Denys Zosimovych <unsinedz.work@gmail.com>
2020-06-12 00:40:06 +0300

    Defined database schema

---
+++ src/Imagegram.Database/Schema.sql
@@ -0,0 +1,23 @@
+create table [dbo].[Accounts] (
+    [Id] uniqueidentifier not null,
+    [Name] nvarchar(100) not null,
+    primary key ([Id])
+);
+
+create table [dbo].[Posts] (
+    [Id] uniqueidentifier not null,
+    [ImageUrl] varchar(100) not null,
+    [CreatorId] uniqueidentifier not null,
+    [CreatedAt] datetime2 not null,
+    primary key ([Id]),
+    foreign key ([CreatorId]) references [dbo].[Accounts]([Id])
+);
+
+create table [dbo].[Comments] (
+    [Id] uniqueidentifier not null,
+    [Content] nvarchar(500) not null,
+    [CreatorId] uniqueidentifier not null,
+    [CreatedAt] datetime2 not null,
+    primary key ([Id]),
+    foreign key ([CreatorId]) references [dbo].[Accounts]([Id])
+);
